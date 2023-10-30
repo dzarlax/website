8e3ea9eef97e8e0ae46e837c3e9ad0dfacf895f2
Alexey Panfilov <alexey@panfilov.net>
2023-10-30 14:57:35 +0100

    Starting dev

---
+++ experience.js
@@ -0,0 +1,26 @@
+export const experience = {
+    ru: [
+        "Технический менеджер проекта, Яндекс.Облако (8/2022 - по настоящее время)",
+        "Менеджер продукта, Яндекс.Маркет (4/2021 - 8/2022)",
+        "Product Owner, Почта России (6/2020 - 4/2021)",
+        "Product Owner, Anywayanyday (2/2019 - 6/2020)",
+        "Старший аналитик | Менеджер продукта, Москва (7/2017 - 2/2019)",
+        "Генеральный директор, Social Technologies, Москва (7/2016 - 8/2017)"
+    ],
+    en: [
+        "Technical Project Manager, Yandex Cloud (8/2022 - Present)",
+        "Product Manager, Yandex.Market (4/2021 - 8/2022)",
+        "Product Owner, Russian Post (6/2020 - 4/2021)",
+        "Product Owner, Anywayanyday (2/2019 - 6/2020)",
+        "Senior Analyst | Product Manager, Moscow (7/2017 - 2/2019)",
+        "Chief Executive Officer, Social Technologies, Moscow (7/2016 - 8/2017)"
+    ],
+    rs: [
+        "Технички Менаџер пројекта, Јандекс Облак (8/2022 - сада)",
+        "Менаџер производа, Јандекс.Маркет (4/2021 - 8/2022)",
+        "Product Owner, Руска пошта (6/2020 - 4/2021)",
+        "Product Owner, Anywayanyday (2/2019 - 6/2020)",
+        "Стари аналитичар | Менаџер производа, Москва (7/2017 - 2/2019)",
+        "Генерални директор, Social Technologies, Москва (7/2016 - 8/2017)"
+    ]
+};
