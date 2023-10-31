6cbe3b6ade8f25681845f152806d1647ef766b41
Alexey Panfilov <alexey@panfilov.net>
2023-10-31 16:52:38 +0100

    Localization rework

--- contacts.js
+++ contacts.js
@@ -11,23 +11,3 @@ function openGithub() {
 }
 
 
-const content_contacts = {
-    en: {
-        email: "Mail me",
-        linkedin: "Check my LinkedIn",
-        github: "My projects",
-        contact: "Contact"
-    },
-    ru: {
-        email: "Напишите мне",
-        linkedin: "Посмотрите мой LinkedIn",
-        github: "Мои проекты",
-        contact: "Контакты"
-    },
-    rs: {
-        email: "Пошаљи ми е-пошту",
-        linkedin: "Проверите мој LinkedIn",
-        github: "Моји пројекти",
-        contact: "Контакт"
-    }
-};
--- localization.js
+++ localization.js
@@ -18,17 +18,24 @@ async function loadLocalizationData(lang) {
 async function switchLang(lang) {
     const localizationData = await loadLocalizationData(lang);
     document.querySelector('#intro h2').textContent = localizationData.intro.title;
-    document.querySelector('#intro p').textContent = content_intro[lang].intro.description;
-    document.querySelector('#skills h2').textContent = content_skills[lang].skills.title;
-    document.querySelector('#experience h2').textContent = content_experience[lang].experience.title;
-    document.querySelector('#contact h2').textContent = menuText[lang].contact;
-    document.querySelector('.contact-buttons .contact-button:nth-child(1)').textContent = content_contacts[lang].email;
-    document.querySelector('.contact-buttons .contact-button:nth-child(2)').textContent = content_contacts[lang].linkedin;
-    document.querySelector('.contact-buttons .contact-button:nth-child(3)').textContent = content_contacts[lang].github;
+    document.querySelector('#intro p').textContent = localizationData.intro.description;
+    document.querySelector('#skills h2').textContent = localizationData.skills.title;
+    document.querySelector('#experience h2').textContent = localizationData.experience.title;
+    document.querySelector('#contact h2').textContent = localizationData.contacts.contact;
+// Получаем кнопки по их id
+    const emailButton = document.getElementById('emailButton');
+    const linkedinButton = document.getElementById('linkedinButton');
+    const githubButton = document.getElementById('githubButton');
+
+// Устанавливаем текст с использованием локализационных данных
+    emailButton.textContent = localizationData.contacts.email;
+    linkedinButton.textContent = localizationData.contacts.linkedin;
+    githubButton.textContent = localizationData.contacts.github;
+
 
     const skillsDiv = document.querySelector('#skills div');
     skillsDiv.innerHTML = "";
-    content_skills[lang].skills.items.forEach(item => {
+    localizationData.skills.items.forEach(item => {
         const span = document.createElement('span');
         span.textContent = item;
         span.className = "skill-tag";
@@ -37,7 +44,7 @@ async function switchLang(lang) {
 
     const experienceList = document.querySelector('#experience ul');
     experienceList.innerHTML = "";
-    content_experience[lang].experience.items.forEach(item => {
+    localizationData.experience.items.forEach(item => {
         const li = document.createElement('li');
         li.textContent = item;
         experienceList.appendChild(li);
@@ -46,8 +53,8 @@ async function switchLang(lang) {
     const menuItems = document.querySelectorAll('[data-lang]');
     menuItems.forEach((item) => {
         const translationKey = item.getAttribute('data-lang'); // Получаем значение атрибута data-lang
-        if (menuText[lang] && menuText[lang][translationKey]) {
-            item.textContent = menuText[lang][translationKey];
+        if (localizationData.menu && localizationData.menu[translationKey]) {
+            item.textContent = localizationData.menu[translationKey];
         }
     });
 }
