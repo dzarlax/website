e3167702fbb649dce022025a5c329a36a1582f70
Alexey Panfilov <alexey@panfilov.net>
2023-10-31 13:12:11 +0100

    Google font optimization

---
+++ optimization.js
@@ -0,0 +1,6 @@
+function loadGoogleFonts() {
+    var link = document.createElement('link');
+    link.href = 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+}
