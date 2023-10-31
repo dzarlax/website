a73538e2cbab520f37a9b9952ccdc1d07f42936e
Alexey Panfilov <alexey@panfilov.net>
2023-10-31 10:24:13 +0100

    Update texts

--- intro.js
+++ intro.js
@@ -2,19 +2,21 @@ const content_intro = {
     ru: {
         intro: {
             title: "Менеджер продукта",
-            description: "8+ лет опыта инноваций в области технологий..."
+            description: "Управление продуктами и проектами, основанными на данных, с более чем 8-летним опытом стимулирования технологических инноваций. Интегрирую инструменты аналитики с знаниями UI/UX для создания функциональных приложений, которые просты в использовании. Имею опыт успешной работы в постоянно меняющихся условиях и применяю навыки оптимизации процессов. Умею четко ставить задачи, проводить анализ и формулировать предложения по ценности. Свободно владею русским и английским, имею базовые знания сербского."
+
         }
     },
     en: {
         intro: {
             title: "Product Manager",
-            description: "Data-driven  product and project manager with 8+ years’ experience driving technology innovation. Integrate analytics tools with UI/UX knowledge to develop feature-rich apps that are simple to use. Track record of thriving in constantly evolving environments and leveraging process optimization skills. Adept at communicating goals, analyses, and value propositions. Fluent in Russian and English, basic knowledge of French and Italian."
+            description: "Data-driven  product and project manager with 8+ years’ experience driving technology innovation. Integrate analytics tools with UI/UX knowledge to develop feature-rich apps that are simple to use. Track record of thriving in constantly evolving environments and leveraging process optimization skills. Adept at communicating goals, analyses, and value propositions. Fluent in Russian and English, basic knowledge of Serbian.",
         }
     },
     rs: {
         intro: {
             title: " Продукт Менаџер",
-            description: "8+ година искуства у покретању технолошке иновације..."
+            description: "Управљање производима и пројектима засновано на подацима са више од 8 година искуства у погону технолошке иновације. Интегришем алатке за аналитику са знањем о UI/UX да би развио апликације богате функцијама које су једноставне за коришћење. Имам доказану способност рада у стално мењајућим срединама и коришћења вештина оптимизације процеса. Способан за комуницирање циљева, анализа и предлога вредности. Течно говорим руски и енглески, основно знање српског."
+
         }
     }
 }
