1479f3930e9eb7a9db4018af6c9d44c377ed6fd5
Alexey Panfilov <alexey@panfilov.net>
2023-11-09 13:32:40 +0100

    Add pubdate

--- rss/display.js
+++ rss/display.js
@@ -1,25 +1,35 @@
 export function displayItems(items) {
-    let html = '<ul>';
-    items.forEach(el => {
-      let description = el.querySelector("description").textContent;
-      let div = document.createElement('div');
-      div.innerHTML = description;
-  
-      div.querySelectorAll('img').forEach(img => {
-        if (img.src === "https://yastatic.net/s3/distribution/stardust/browser-summary-web/1.9.0/_app/immutable/assets/link.97114a7f.svg") {
-          img.remove();
-        } else {
-          img.classList.add('rss-image');
-        }
-      });
-  
-      description = div.innerHTML;
-      html += `<li><h2>${el.querySelector("title").textContent}</h2><p>${description}</p></li>`;
+  let html = '<ul>';
+  items.forEach(el => {
+    // Extract the title, description, and publication date from the RSS item
+    let title = el.querySelector("title").textContent;
+    let description = el.querySelector("description").textContent;
+    let pubDate = new Date(el.querySelector("pubDate").textContent).toLocaleString(); // This will format the date and time
+
+    // Create a div element to manipulate the HTML description
+    let div = document.createElement('div');
+    div.innerHTML = description;
+
+    // Remove unwanted images and add a class to the others
+    div.querySelectorAll('img').forEach(img => {
+      if (img.src === "https://yastatic.net/s3/distribution/stardust/browser-summary-web/1.9.0/_app/immutable/assets/link.97114a7f.svg") {
+        img.remove();
+      } else {
+        img.classList.add('rss-image');
+      }
     });
-    html += '</ul>';
-    this.container.innerHTML = html;
-    this.displayPagination();
-  }
+
+    // Update the description after image manipulation
+    description = div.innerHTML;
+
+    // Add the title, formatted description, and publication date to the HTML string
+    html += `<li><h2>${title}</h2><h4>${pubDate}</h4><p>${description}</p></li>`;
+  });
+  html += '</ul>';
+  this.container.innerHTML = html;
+  this.displayPagination();
+}
+
   
   export function displayPagination() {
     this.container.innerHTML += this.createPagination();
